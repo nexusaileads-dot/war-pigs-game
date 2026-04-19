2c85d8f6aef2c40aab0eefcb2f8f47ffe4dad22b
nexusaileads-dot <nexusaileads@gmail.com>
2026-04-19 13:36:32 +0100

    Update PigPlayer.ts

--- apps/web/src/game/entities/PigPlayer.ts
+++ apps/web/src/game/entities/PigPlayer.ts
@@ -1,25 +1,42 @@
 import Phaser from 'phaser';
 
+type MovementKeys = {
+  up?: Phaser.Input.Keyboard.Key;
+  down?: Phaser.Input.Keyboard.Key;
+  left?: Phaser.Input.Keyboard.Key;
+  right?: Phaser.Input.Keyboard.Key;
+};
+
 export class PigPlayer extends Phaser.Physics.Arcade.Sprite {
-  constructor(scene: Phaser.Scene, x: number, y: number) {
-    super(scene, x, y, 'player');
+  constructor(scene: Phaser.Scene, x: number, y: number, texture: string = 'player') {
+    super(scene, x, y, texture);
+
     scene.add.existing(this);
     scene.physics.add.existing(this);
+
     this.setCollideWorldBounds(true);
+    this.setOrigin(0.5, 0.5);
   }
 
-  updateMovement(cursors: Phaser.Types.Input.Keyboard.CursorKeys, wasd: any, speed: number) {
+  updateMovement(
+    cursors: Phaser.Types.Input.Keyboard.CursorKeys,
+    wasd: MovementKeys,
+    speed: number
+  ) {
     let vx = 0;
     let vy = 0;
-    if (cursors.left.isDown || wasd.left.isDown) vx = -speed;
-    if (cursors.right.isDown || wasd.right.isDown) vx = speed;
-    if (cursors.up.isDown || wasd.up.isDown) vy = -speed;
-    if (cursors.down.isDown || wasd.down.isDown) vy = speed;
+
+    if (cursors.left?.isDown || wasd.left?.isDown) vx = -speed;
+    if (cursors.right?.isDown || wasd.right?.isDown) vx = speed;
+    if (cursors.up?.isDown || wasd.up?.isDown) vy = -speed;
+    if (cursors.down?.isDown || wasd.down?.isDown) vy = speed;
 
     if (vx !== 0 && vy !== 0) {
-      vx *= 0.707;
-      vy *= 0.707;
+      const diagonalFactor = Math.SQRT1_2;
+      vx *= diagonalFactor;
+      vy *= diagonalFactor;
     }
+
     this.setVelocity(vx, vy);
   }
 }
