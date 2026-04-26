44727eec193ad5ab4cc38f792723623825fdbf9f
nexusaileads-dot <nexusaileads@gmail.com>
2026-04-26 05:45:11 +0100

    Update main.tsx

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/global.css';
+import { GameNoticeProvider } from './components/GameNoticeProvider';
 
 declare global {
   interface Window {
@@ -38,6 +39,8 @@ if (!rootElement) {
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <App />
+    <GameNoticeProvider>
+      <App />
+    </GameNoticeProvider>
   </React.StrictMode>
 );
